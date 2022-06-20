c46dd45b6d917fafb45d7750c5cfeafc25a2a130
LoganStyles <emmaist23@gmail.com>
2022-06-20 07:54:34 +0100

    feat(Recursion) added comments to the Sum of arrays problem

--- Recursion/Sum.cs
+++ Recursion/Sum.cs
@@ -5,9 +5,45 @@
      * as an input. the function should return the total sum
      * of all elements.
      */
+
+    //start by listing out test cases of varying sizes
+    /* 
+     Solution([]) = 0
+     Solution([1]) = 1
+     Solution([1,5]) = 6
+     Solution([1,5,7]) = 13
+     Solution([1,5,7,-2]) = 11
+
+     take note:
+     we structure the example in such a way as to have varying input size together with overlapping elements
+     
+     this leads us towards solving this problem recursively
+     where the empty array is a base case.
+     Solution([]) = 0
+
+     next we can highlight any 2 instances to indicate the recursive cases:
+     Solution([1,5]) = 6
+     Solution([1,5,7]) = 13
+     ----the above can b represented recursively below
+     Solution([1,5,7]) == 7 + Solution([1,5])
+     
+     in general, we now have:
+     Solution([a,b,c,d]) = a + Solution([b,c,d])
+
+     we can then re-evaluate the math from bottom up
+
+     Solution([]) => 0
+     Solution([1]) = 1 + Solution([]) => 1
+     Solution([1,5]) = 5 + Solution([1]) => 6
+     Solution([1,5,7]) = 7 + Solution([1,5]) => 13
+     Solution([1,5,7,-2]) = -2 + Solution([1,5,7]) => 11
+     here 11 is the final answer
+
+
+      */
     internal class Sum
     {
-        //Time: O(n^2)
+        //Time: O(n^2): we have O(n) recursive calls, within each (in the worst case) we perform O(n) iterations due to Array.Copy
         //Space: O(n) from coderbyte video??, should'nt it be O(n^2) due to additional array created
         public int Solution(int[] input)
         {
@@ -20,6 +56,7 @@
             return input[0] + Solution(remaining);
         }
 
+        // this solution removes the additional iteration due to Array.Copy
         //Time: O(n)
         //Space: O(n)
         public int SolutionWithIndex(int[] input)
@@ -27,6 +64,8 @@
             return SolutionWithIndexHelper(input, 0);
         }
 
+        //here index represents the start of the array under consideration
+        //hence no extra array is needed
         private int SolutionWithIndexHelper(int[] input, int index)
         {
             if (index == input.Length)
